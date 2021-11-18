4fe42acafd2378f0813f9e4d29538f61c3295726
손영배 <dudqo225@naver.com>
2021-11-18 17:50:06 +0900

    profile.vue add

---
+++ src/views/accounts/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    프로필
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data: function () {
+    return {
+      user: {},
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style>
+
+</style>
